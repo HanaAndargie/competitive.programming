f5b2b4a1c12964ff750ffc5a6f6567c946d6b14b
HanaAndargie <81221258+HanaAndargie@users.noreply.github.com>
2022-10-12 10:20:05 +0300

    Create  totalFruit.py

---
+++ totalFruit.py
@@ -0,0 +1,20 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        start=0
+        curr_fruits=0
+        max_fruits=0
+        basket={}
+        
+        for end in range(len(fruits)):
+            curr_fruits+=1
+            basket[fruits[end]]=basket.get(fruits[end],0)+1
+            
+            while len(basket)>2:
+                curr_fruits-=1
+                basket[fruits[start]]-=1
+                
+                if basket[fruits[start]]==0:
+                    del basket[fruits[start]]
+                start+=1
+            max_fruits=max(max_fruits,curr_fruits)
+        return max_fruits
